0f8537a3af455c0115f7e1e1edd7a68d04824e0a
John Skandalakis <skandaloptagon@gmail.com>
2017-03-29 20:25:43 -0400

    Stock tickers

--- get_top_reddit_posts.py
+++ get_top_reddit_posts.py
@@ -11,21 +11,22 @@ reddit = praw.Reddit(client_id='HDUejy8lyaO-uw',
                      client_secret='dkQB2xSpT9GpqWwZcePnynZVd6g',
                      user_agent='Linux:BuzzWordTrading:0.1 (by /u/BuzzWordTrading)')
 
-go_list = []
+go_list = {}
 
 with open("constituents.csv","rb") as f:
     csv_file = csv.reader(f)
     for row in csv_file:
         #go_list.append(row[0])
-        go_list.append(row[1].lower())
+        go_list[row[1].lower()]=row[0]
 
-go_list = set(go_list)
 
-for submission in reddit.subreddit(args.sub).top(limit=999999):
+for submission in reddit.subreddit(args.sub).top(limit=1000):
+    #if submission.score < 5000:
+    #    continue
     text = submission.title + ' ' + submission.selftext
     text = text.lower()
     for check in [(go_word in text, go_word) for go_word in go_list]:
         if check[0]:
             created = time.strftime("%Y-%m-%d", time.localtime(submission.created))
-            print check[1], submission.shortlink, created 
+            print go_list[check[1]], submission.shortlink, created 
         
